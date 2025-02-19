ab8f633dabed1d92915d3f9cb15d4a6cdf228bd7
Maksim Chalyh <nazgulius@yandex.ru>
2025-02-19 17:18:06 +0300

    html OK, first command for sesrver OK

--- src/js/helpdesk/frontend/src/js/HelpDesk.js
+++ src/js/helpdesk/frontend/src/js/HelpDesk.js
@@ -14,10 +14,42 @@ export default class HelpDesk {
     console.info('init');
 
     const btnAddTicket = document.querySelector('.btn_add_ticket');
+    const btnCensel = document.querySelector('.form_btn_censel');
+    const mein = document.querySelector('.mein');
+    const form = mein.querySelector('.form');
+    const titleInput = mein.querySelector('.form_title_1');
+    const textInput = mein.querySelector('.form_title_2');
 
+    // показать форму
     btnAddTicket.addEventListener('click', (e) => {
-      console.log('click!');
-      
+      e.preventDefault();
+      form.classList.remove('hidden');      
     });
+    
+    // скрыть форму
+    btnCensel.addEventListener('click', (e) => {
+      e.preventDefault();
+      form.classList.add('hidden');      
+    });
+
+    // основной слушатель формы
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const xhr = new XMLHttpRequest();
+
+      xhr.onreadystatechange = function(e) {
+        if (xhr.readyState != 4) return;
+        
+        console.log(xhr.responseText);
+
+      }
+
+      xhr.open('GET', 'http://localhost:7070/?method=allTickets');
+      
+      xhr.send();
+    });
+
+
   }
 }
