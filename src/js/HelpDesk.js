8ee84a0f9fb1cb3b42d7850a2c3321adeef924ab
“Maksim <nazgulius@yandex.ru>
2025-02-22 22:36:49 +0300

    xhr send and load OK

--- src/js/HelpDesk.js
+++ src/js/HelpDesk.js
@@ -3,6 +3,7 @@
  * */
 import TicketForm from './TicketForm';
 import Ticket from './Ticket';
+import TicketView from './TicketView';
 
 export default class HelpDesk {
   constructor(container, ticketService) {
@@ -11,20 +12,20 @@ export default class HelpDesk {
     }
     this.container = container;
     this.ticketService = ticketService;
-    this.ticketForm = new TicketForm();    
+    this.ticketForm = new TicketForm(); 
+    this.ticketView = new TicketView();   
   }
 
   init() {
     console.info('init');
     this.ticketForm.createForm();
 
-
     const btnAddTicket = document.querySelector('.btn_add_ticket');
     const btnCensel = document.querySelector('.form_btn_censel');
     const mein = document.querySelector('.mein');
-    const form = mein.querySelector('.form');
-    const titleInput = mein.querySelector('.form_title_1');
-    const textInput = mein.querySelector('.form_title_2');
+    const form = document.querySelector('.form');
+    let data;
+
 
     // показать форму
     btnAddTicket.addEventListener('click', (e) => {
@@ -36,45 +37,59 @@ export default class HelpDesk {
     btnCensel.addEventListener('click', (e) => {
       e.preventDefault();
       form.classList.add('hidden');
+      form.querySelector('.form_input_1').value = '';
+      form.querySelector('.form_input_2').value = '';
     });
-
-    // this.ticket.id = null;
-    // this.ticket.name = titleInput;
-    // this.ticket.description = textInput;
-    // this.ticket.status = false;
-    // this.ticket.created = new Date();
-
     
     let xhr = new XMLHttpRequest();
     
     // основной слушатель формы
     form.addEventListener('submit', (e) => {
-      e.preventDefault();
+      e.preventDefault(); 
+      const titleInput = form.querySelector('.form_input_1');
+      const textInput = form.querySelector('.form_input_2');
       
-      const ticket = new Ticket(null, titleInput, textInput, false, new Date());
+      const ticket = new Ticket({
+        id: null,
+        name: titleInput.value,
+        description: textInput.value,
+        status: false,
+        created: new Date(),
+      });
 
       xhr.onreadystatechange = function () {
         if (xhr.readyState !== 4) return;
         console.log(xhr.responseText);
       };
 
+      console.log(ticket);
       let json = JSON.stringify(ticket);
+      console.log(json);
 
       //xhr.open("POST", '/submit')
       // xhr.open('GET', 'http://localhost:7070/?method=allTickets');
       xhr.open('POST', 'https://ahj-homeworks-helpdesk-server.onrender.com/?method=createTicket');
       xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
       xhr.send(json);
+
+      form.classList.add('hidden');
+      titleInput.value = '';
+      textInput.value = '';
     });
 
     xhr.addEventListener('load', () => {
       if (xhr.status >= 200 && xhr.status < 300) {
-          try {
-              const data = JSON.parse(xhr.responseText);
-              console.log(data);
-          } catch (e) {
-              console.error(e);
+        try {
+          data = JSON.parse(xhr.responseText);
+          console.log(data);
+          if (data) {
+            this.ticketView.createTicket(data.id, data.name, data.description, data.status, data.created);             
+          } else {  
+            console.error('ticketData is undefined or null');  
           }
+        } catch (e) {
+          console.error(e);
+        }
       }
     });
 
--- src/js/TicketView.js
+++ src/js/TicketView.js
@@ -3,25 +3,21 @@
  *  Он содержит методы для генерации разметки тикета.
  * */
 export default class TicketView {
-  constructor(subTitle, subText) {
-    this.subTitle = subTitle;
-    this.subText = subText;
+  constructor(id, name, description, status, created) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.status = status;
+    this.created = created;
   }
 
-  createTicket() {
-    console.log(`subTitle ${this.subText}
-subText ${this.subText}`);
+  createTicket(id, name, description, status, created) {
+    console.log(`${this.id} = id;
+${this.name} = name;
+${this.description} = description;
+${this.status} = status;
+${this.created} = created;`);
 
-// const form = document.createElement('form');
-// form.className = 'form hidden';
-// form.method = 'POST';
-// form.action = this.url;
-
-// const formTitle = document.createElement('h3');
-// formTitle.className = 'form_title';
-// formTitle.textContent = 'Добавить тикет';
-
-// form.appendChild(formTitle);
     const ticketItem = document.createElement('li');
     ticketItem.className = 'ticket_item';
 
@@ -30,24 +26,27 @@ subText ${this.subText}`);
 
     const ticketTitle = document.createElement('p');
     ticketTitle.className = 'ticket_title';
-    ticketTitle.textContent = this.subTitle;
+    ticketTitle.textContent = name;
 
     const ticketData = document.createElement('span');
     ticketData.className = 'ticket_data';
-    ticketData.textContent = 'Date: ';
+    ticketData.textContent = 'Date: ' + created;
     
     const dotEdit = document.createElement('div');
     dotEdit.className = 'dot_edit circle';
 
     const dotClose = document.createElement('div');
     dotClose.className = 'dot_close circle';
-    dotClose.textContent = 'X';
+    const dotCloseX = document.createElement('span');
+    dotCloseX.className = 'dot_close_x';
+    dotCloseX.textContent = 'X';
 
     ticketItem.appendChild(dotStatus);
     ticketItem.appendChild(ticketTitle);
     ticketItem.appendChild(ticketData);
     ticketItem.appendChild(dotEdit);
+    dotClose.appendChild(dotCloseX);
     ticketItem.appendChild(dotClose);
-
+    document.querySelector('.ticket_list').appendChild(ticketItem);
   }
 }
